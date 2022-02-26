51e24f0adb414329e3dd9c4183475e5853291465
Suchitra <suchitrasrinivas10@gmail.com>
2022-02-26 15:56:53 +0530

    Updated the code to call currency-exchange-service

--- currency-conversion-service/currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,23 +1,36 @@
 package com.example.microservices.currencyconversionservice.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.microservices.currencyconversionservice.model.CurrencyConversion;
 
 @RestController
 public class CurrencyConversionController {
-	
-	
+
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion convertCurrency(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
-		
-		return new CurrencyConversion(1001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, " ");
-		
+
+		HashMap<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		ResponseEntity<CurrencyConversion> response = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class,
+				uriVariables);
+		CurrencyConversion currencyConversion = response.getBody();
+
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+				currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
+
 	}
 
 }
